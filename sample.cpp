4ede21c3c4812577d13394f01006dc494593e6b6
tasssystyle <tasssystyle@yhoo.co.jp>
2014-05-19 18:21:30 +0900

    sample

---
+++ sample.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x,y;
+	
+//	x = 265;
+
+//	y = 15;
+
+	for (x = 265; x <= 385; x+=4)
+    
+		printf("x=%d\n", +x);
+
+	for (y = 15; y <= 135; y+=4)
+
+		printf("y=%d\n",+y);
+	
+/*	x = x +4;
+	
+	printf("x=%5d\n",+x);
+	
+	x = x + 4;
+
+	printf("x=%5d", +x);
+	if ("")*/
+	
+	return 0;
+}
